1b6d742bb1de62eacfb387a66b466852b063635f
ThiagoA7X19 <thiagopcosta08@gmail.com.br>
2026-03-10 13:51:17 -0300

    teste

--- cloneGit.cpp
+++ cloneGit.cpp
@@ -7,12 +7,13 @@ int main() {
     setlocale(LC_ALL, "pt_BR.utf8"); //Para usar acentuação
     cout << "\033[1;34m***************************************************\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
-    cout << "\033[1;32m*   \033[1;33m PARABÉNS 34DS!                     \033[1;32m          *\033[0m\n";
+    cout << "\033[1;32m*   \033[1;33m EU SOU DO ROOOOOOCKKKK!                     \033[1;32m          *\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
-    cout << "\033[1;32m*   \033[1;36mVocê clonou um repositório público do Git     \033[1;32m*\033[0m\n";
-    cout << "\033[1;32m*   \033[1;36mpara o seu repositório local!                 \033[1;32m*\033[0m\n";
+    cout << "\033[1;32m*   \033[1;36mVocê clonei um repositório público do Git     \033[1;32m*\033[0m\n";
+    cout << "\033[1;32m*   \033[1;36mpara o meu repositório local!                 \033[1;32m*\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
     cout << "\033[1;34m***************************************************\033[0m\n";
 
     return 0;
 }
+//EU SOU MUITO BOM EM C++!
